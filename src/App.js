e72ef605593ba29f75f6d0bc491cecf3cc6bfcff
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-12-13 13:35:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import AllRoutes from "./Routes/AllRoutes";
+// import Navbar from "./Components/Navbar";
+// import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
